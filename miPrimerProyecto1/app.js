7a521b6a9639b24f9a5a6b9fc324bec899df15f5
Atch2 <anthonyfabian212@gmail.com>
2024-01-24 19:32:57 -0500

    firt commit

---
+++ miPrimerProyecto1/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const puerto = 3001;
+
+app.get("/contactos", (request, response)=>{
+    response.send("get contactos");
+})
+
+app.post("/contactos",(request, response)=>{
+    response.send("post contactos")
+})
+
+app.listen(puerto , ()=> {
+    console.log("Servidor listo en el puerto 3001")
+} );
